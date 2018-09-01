e8daee46b9d0444c2f30dc0c22bf4d55c8b7be2c
kelseyhealey <42752573+kelseyhealey@users.noreply.github.com>
2018-09-01 09:40:58 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,37 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-<body>
-
-<h1>My First Webpage</h1>
-
-<p>Hello I am Kelsey</p>
-
- </head>
+	<title>CDCore</title>
+		<link rel="stylesheet" type="text/css" href="css/cdcorecss.css">
+	<style>
+		a { 
+    	color: white;
+		text-decoration: none;
+		}
+	</style>
+</head>
+	<header>
+		<nav>
+				<ul>
+				<a href="https://khealey.azurewebsites.net/OneSourceAdmin.html">OneSourceAdmin</a>&nbsp;
+				<a href="https://khealey.azurewebsites.net/CDRequests.html">CD Requests</a>&nbsp;
+				<a href="https://khealey.azurewebsites.net/RequestRuleResults.html">Request Rule Results</a>&nbsp;
+				<a href="https://khealey.azurewebsites.net/RejectedRequests.html">Rejected Results</a>&nbsp;
+				<a href="https://khealey.azurewebsites.net/SystemInfo.html">System Info</a>&nbsp;
+				<a href="https://khealey.azurewebsites.net/Configuration.html">Configuration</a>&nbsp;
+				<a href="https://khealey.azurewebsites.net/MarketPayerDataImport.html">MarketPayer Data Import</a>&nbsp;
+				</ul>
+			</nav>
+		<section id="head">
+			<p>User: Kelsey Healey     Role: Power User</p>
+			</section>
+	</header>
+<body>	
+	<section id="main">
+		<p>Coverage Discovery Admin Site</p><p>Welcome!</p>
+	</section>
 </body>
+		<footer>
+			&copy; 2018 CDCore - Kelsey Healey
+		</footer>
 </html>
