1dcd1a4650316d93c630ad189dcebeee67d1cffb
kelseyhealey <42752573+kelseyhealey@users.noreply.github.com>
2018-09-01 09:48:23 -0500

    Create CDRequests.php

---
+++ CDRequests.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CDCore</title>
+		<link rel="stylesheet" type="text/css" href="css/cdcorecss.css">
+	<style>
+		a { 
+    	color: white;
+		text-decoration: none;
+		}
+	</style>
+</head>
+	<header>
+		<nav>
+				<ul>
+				<a href="https://khealey.azurewebsites.net/OneSourceAdmin.html">OneSourceAdmin</a>&nbsp;
+				<a href="https://khealey.azurewebsites.net/CDRequests.html">CD Requests</a>&nbsp;
+				<a href="https://khealey.azurewebsites.net/RequestRuleResults.html">Request Rule Results</a>&nbsp;
+				<a href="https://khealey.azurewebsites.net/RejectedRequests.html">Rejected Results</a>&nbsp;
+				<a href="https://khealey.azurewebsites.net/SystemInfo.html">System Info</a>&nbsp;
+				<a href="https://khealey.azurewebsites.net/Configuration.html">Configuration</a>&nbsp;
+				<a href="https://khealey.azurewebsites.net/MarketPayerDataImport.html">MarketPayer Data Import</a>&nbsp;
+				</ul>
+		</nav>
+			<section id="head">
+				<p>User: Kelsey Healey     Role: Power User</p>
+			</section>
+	</header>
+<body>	
+		<section id="main">
+			</section>
+			<div class= "body">
+				<p>CD Requests</p>
+			</div>
+</body>
+		<footer>
+			&copy; 2018 CDCore - Kelsey Healey
+		</footer>
+</html>
