36fb52e7a26a08282b4bc131c56477fde4a4032d
kelseyhealey <42752573+kelseyhealey@users.noreply.github.com>
2018-09-05 14:55:35 -0500

    Update dbinfo.ini.php

--- dbinfo.ini.php
+++ dbinfo.ini.php
@@ -1,10 +1,9 @@
 ;<?php
 ;die();
 ;/*
-[database]
-username = uid
-password = pass
-dbname = name
-#these will have ctual credentials filled in just not shared on GitHub
+[Database]
+username = khealey
+password = Zsxdcfa5!
+dbname = CDCore
 ;*/
 ;?>
